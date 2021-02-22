2fbf5e7a17e114c6b635cb8037bc0e7e342b453a
ShobhitSagar <shobhitsagar109@gmail.com>
2021-02-22 17:04:32 +0530

    Showing in RecyclerView. Using Glide and CircleImageView.

--- app/src/main/java/com/devss/tmdbapp/MainActivity.java
+++ app/src/main/java/com/devss/tmdbapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.devss.tmdbapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -21,18 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    //vars
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    // https://image.tmdb.org/t/p/w185
+
     public static String BASE_URL = "https://api.themoviedb.org";
-    public static int PAGE = 1;
     public static String API_KEY = "80042d5849e24d561f9ccd7beed28805";
     public static String LANGUAGE = "en-us";
     public static String CATEGORY = "popular";
+    public static int PAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView listView = findViewById(R.id.list_view);
+//        ListView listView = findViewById(R.id.list_view);
 
         Retrofit retrofit = new Retrofit.Builder()
                             .baseUrl(BASE_URL)
@@ -52,21 +59,29 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onResponse: "+ listOfMovies.get(3).getTitle());
 
                 // In ListView
-                List<String> list = new ArrayList<>();
+//                List<String> list = new ArrayList<>();
                 for (int i=0; i<listOfMovies.size(); i++) {
-                    list.add(listOfMovies.get(i).getTitle());
+                    mNames.add(listOfMovies.get(i).getTitle());
+                    mImageUrls.add("https://image.tmdb.org/t/p/w185"+listOfMovies.get(i).getPosterPath());
                 }
 
-                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
-                listView.setAdapter(adapter);
+//                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
+//                listView.setAdapter(adapter);
+
+                initRecyclerView();
             }
 
             @Override
             public void onFailure(Call<MovieResults> call, Throwable t) {
-
+                Log.d(TAG, "onFailure: "+t);
             }
         });
+    }
 
-
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.list_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(MainActivity.this, mNames, mImageUrls);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
